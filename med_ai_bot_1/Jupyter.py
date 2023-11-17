e79595c2406cc5ed967e2a75db4b616fd8816b79
Valera386 <valeraorigina@gmail.com>
2023-11-17 03:22:10 +0300

    jupyter-4

--- med_ai_bot_1/Jupyter.py
+++ med_ai_bot_1/Jupyter.py
@@ -13,13 +13,21 @@ def load_csv(file_path):
             data.append(row['Жалобы'])
     return data
 
+def write_file(file_path_w, text):
+    with open(file_path_w, 'w', encoding="utf8") as file:
+
+        for text_out, count in text:
+            file.write(f"{text_out}: {count} раз(а)\n")
+
+    return text
+
 def filter_sentences(sentences):
-    filtered_sentences = [sentence for sentence in sentences if 'нет' not in sentence.lower() and 'не предьявляет' not in sentence.lower() and 'не имеет' not in sentence.lower() and 'не предъявляет' not in sentence.lower()]
+    filtered_sentences = [sentence for sentence in sentences if 'нет' not in sentence.lower() and 'не предьявляет' not in sentence.lower() and 'не имеет' not in sentence.lower() and 'не предъявляет' not in sentence.lower()and 'медосмотр' not in sentence.lower()] 
     
     return filtered_sentences
 
 def lemmatize_text(text):
-    # Здесь ты можешь добавить свой код для лемматизации или другой обработки текста
+    #лемматизация или другой обработки текста
     # В данном примере, используется простая лемматизация, убирающая пробелы и приводящая к нижнему регистру
     return text.lower()
 
@@ -32,16 +40,29 @@ def find_duplicates(data):
 
     return top_10_duplicates
 
-if __name__ == "__main__":
-    file_path = '/home/log/med_ai_bot_1/med_ai_bot_1/database/123.csv'
+def top_ten(file_path): #функция находит топ 10 самых частых жалоб
+    file_path_write = '/home/log/med_ai_bot_1/med_ai_bot_1/Задание второго этапа Сириус.ИИ/4.Топ_10_Жалоб.txt'
+
     data = load_csv(file_path)
 
     filter_data = filter_sentences(data)
 
-    #print(filter_data)
-
     top_duplicates = find_duplicates(filter_data)
 
+    top_duplicates = write_file(file_path_write, top_duplicates)
+
+    return top_duplicates
+
+
+if __name__ == "__main__":
+    file_path_dataset = '/home/log/med_ai_bot_1/med_ai_bot_1/database/med.csv'
+    
+    top_duplicates = top_ten(file_path_dataset)
+
     print("Топ-10 повторяющихся жалоб:")
-    for text, count in top_duplicates:
-        print(f"{text}: {count} раз(а)")
+
+    for text_out, count in top_duplicates:
+        print(f"{text_out}: {count} раз(а)")
+
+    
+    
