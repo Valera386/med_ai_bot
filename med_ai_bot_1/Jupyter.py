5a56d2c59e619f197f4b13df4ad51d0f8f970baa
Valera386 <valeraorigina@gmail.com>
2023-11-17 03:54:01 +0300

    jupyter-2

--- med_ai_bot_1/Jupyter.py
+++ med_ai_bot_1/Jupyter.py
@@ -13,13 +13,30 @@ def load_csv(file_path):
             data.append(row['Жалобы'])
     return data
 
-def write_file(file_path_w, text):
+def write_file_four(file_path_w, text): #запись топ-10 жалоб
     with open(file_path_w, 'w', encoding="utf8") as file:
 
         for text_out, count in text:
             file.write(f"{text_out}: {count} раз(а)\n")
 
     return text
+
+def write_file_two(file_path_w, text): #запись топ-10 жалоб
+    with open(file_path_w, 'w', encoding="utf8") as file:
+
+        file.write(f"Жалобы;\n")
+        for text_out, count in text:
+            file.write(f"{text_out};\n")
+
+
+def sort_complaints_by_frequency(complaints):
+    # Используем Counter для подсчета частотности каждой жалобы
+    complaints_counter = Counter(complaints)
+    
+    # Сортируем жалобы по частотности встречаемости (от наиболее частых к менее частым)
+    sorted_complaints = sorted(complaints_counter.items(), key=lambda x: x[1], reverse=True)
+    
+    return sorted_complaints
 
 def filter_sentences(sentences):
     filtered_sentences = [sentence for sentence in sentences if 'нет' not in sentence.lower() and 'не предьявляет' not in sentence.lower() and 'не имеет' not in sentence.lower() and 'не предъявляет' not in sentence.lower()and 'медосмотр' not in sentence.lower()] 
@@ -40,6 +57,18 @@ def find_duplicates(data):
 
     return top_10_duplicates
 
+def list_complaint(file_path):
+    file_path_write = '/home/log/med_ai_bot_1/med_ai_bot_1/Задание второго этапа Сириус.ИИ/2.Жалобы.csv'
+
+    data = load_csv(file_path)
+
+    sorted_complaints = sort_complaints_by_frequency(data)
+
+    write_file_two(file_path_write, sorted_complaints)
+
+    return sorted_complaints
+
+
 def top_ten(file_path): #функция находит топ 10 самых частых жалоб
     file_path_write = '/home/log/med_ai_bot_1/med_ai_bot_1/Задание второго этапа Сириус.ИИ/4.Топ_10_Жалоб.txt'
 
@@ -49,7 +78,7 @@ def top_ten(file_path): #функция находит топ 10 самых ча
 
     top_duplicates = find_duplicates(filter_data)
 
-    top_duplicates = write_file(file_path_write, top_duplicates)
+    top_duplicates = write_file_four(file_path_write, top_duplicates)
 
     return top_duplicates
 
@@ -59,10 +88,16 @@ if __name__ == "__main__":
     
     top_duplicates = top_ten(file_path_dataset)
 
+    sorted_complaints = list_complaint(file_path_dataset)
+
     print("Топ-10 повторяющихся жалоб:")
 
     for text_out, count in top_duplicates:
         print(f"{text_out}: {count} раз(а)")
+    
+    print('\n\n-----------------------------------------------------------\n\n')
+
+    
 
     
     
